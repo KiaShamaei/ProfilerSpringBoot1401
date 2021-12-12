f31945605c25813752928a7048dfed49ed767f21
kiarash.shamaii@gmail.com <kiarash.shamaii@gmail.com>
2021-12-12 07:24:14 +0330

    init project

---
+++ blog/src/test/java/kia/academy/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package kia.academy.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
