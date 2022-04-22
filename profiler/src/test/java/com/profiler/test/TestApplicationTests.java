3966f46a239c99ccdbefe8169dfaecb40a41f32d
kiarash.shamaii@gmail.com <kiarash.shamaii@gmail.com>
2022-04-22 09:32:12 +0430

    profile with yml

---
+++ profiler/src/test/java/com/profiler/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profiler.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
